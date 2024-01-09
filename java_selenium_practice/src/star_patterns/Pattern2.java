8d1b4fdbe534b3c41c23ad1028cd4a574ba8e659
vinusuresh <vineethsuresh77@gmail.com>
2024-01-09 12:59:16 +0530

    added pattern2 program

---
+++ java_selenium_practice/src/star_patterns/Pattern2.java
@@ -0,0 +1,19 @@
+package star_patterns;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		int num = 1;
+		for(int i = 1; i<=5; i++)
+		{
+			for(int j =1; j<= 5; j++)
+			{
+				System.out.print(num);
+			}
+			System.out.println();
+			num = num+1;
+		}
+
+	}
+
+}
