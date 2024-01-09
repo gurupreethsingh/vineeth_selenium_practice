095915ef45449522a0e71e983084342899eee7a9
vinusuresh <vineethsuresh77@gmail.com>
2024-01-09 18:36:09 +0530

    pattern 4 pgm

---
+++ java_selenium_practice/src/star_patterns/Pattern4.java
@@ -0,0 +1,17 @@
+package star_patterns;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		int num = 65;
+		for(int i = 1; i<=5; i++)
+		{
+			for(int j =1; j<= 5; j++)
+			{
+				System.out.print((char)num);
+			}
+			System.out.println();
+			num = num+1;
+		}
+	}
+}
