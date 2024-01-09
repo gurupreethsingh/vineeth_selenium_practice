d37c76f654436d8c054d002188044838fc698447
vinusuresh <vineethsuresh77@gmail.com>
2024-01-09 18:47:02 +0530

    ptrn5

--- java_selenium_practice/src/star_patterns/Pattern5.java
+++ java_selenium_practice/src/star_patterns/Pattern5.java
@@ -1,9 +1,9 @@
 package star_patterns;
 
-public class Pattern4 {
+public class Pattern5 {
 
 	public static void main(String[] args) {
-		int num = 69;
+		int num = 65;
 		for(int i = 1; i<=5; i++)
 		{
 			for(int j =1; j<= 5; j++)
@@ -11,7 +11,7 @@ public class Pattern4 {
 				System.out.print((char)num);
 			}
 			System.out.println();
-			num = num-1;
+			num = num+1;
 		}
 	}
 }
