a50c74805b72df73b68c7b1d60f7120e1c1c3806
gurupreethsingh <igurupreeth@gmail.com>
2024-01-09 12:29:52 +0530

    i have added the constructor code.

---
+++ java_selenium_practice/src/constructor_programs/Whastapp.java
@@ -0,0 +1,21 @@
+// constructor - are used to initialize data members
+// constructor name is same as class name
+// constructors do not have any return type.
+
+package constructor_programs;
+
+public class Whastapp 
+{
+	// constructors are always non static members 
+   Whastapp()
+	{
+		System.out.println("Hello from the constructor");
+	}
+
+	
+	public static void main(String[] args) 
+	{
+		// to access non static member inside the main function. we have to crate an object of that class. 
+		Whastapp w = new Whastapp();
+	}
+}
