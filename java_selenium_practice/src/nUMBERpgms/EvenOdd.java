4299505571fb2bd2d4ed9fb5af0ca352bbf32bb5
vinusuresh <vineethsuresh77@gmail.com>
2024-01-09 13:26:22 +0530

    even vs odd pgm

---
+++ java_selenium_practice/src/nUMBERpgms/EvenOdd.java
@@ -0,0 +1,26 @@
+package nUMBERpgms;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		int num;
+		System.out.println("enter a number");
+		Scanner s=new Scanner(System.in);
+		num=s.nextInt();
+		if(num%2==0)
+		{ 
+			System.out.println("the number is even");
+			
+		}
+		else
+		{
+			System.out.println("the number is odd");
+		}
+		
+		
+
+	}
+
+}
